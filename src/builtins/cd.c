7215c9b5745a9e47bfee713923d48e197e858021
saouissi <sami.aouissi21@gmail.com>
2026-06-07 19:24:30 +0200

    progress on cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,18 +6,42 @@
 /*   By: saouissi <saouissi@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 18:11:38 by saouissi          #+#    #+#             */
-/*   Updated: 2026/06/03 17:37:34 by saouissi         ###   ########.fr       */
+/*   Updated: 2026/06/07 19:20:23 by saouissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static void	dotter(char *a, t_shell *shell)
+static int	nbe(char *name, t_shell *shell)
 {
+	char	**env;
+	int		i;
+
+	i = -1;
+	env = shell->env;
+	while (env[++i])
+	{
+		if (ft_strncmp(env[i], name, ft_strlen(name)) == 0)
+			return (i);
+	}
+	return (0);
+}
+
+static void	dotter(t_shell *shell)
+{
+	char	**splitdir;
+	char	*a;
 	int	x;
 
-	x = ft_strlen(a);
-	if (a[1] == '.')
+	x = 0;
+	if (shell->cmds->cmd_args[0] == '/')
+		a = ft_strdup("/");
+	else if (shell->cmds->cmd_args[0] == '~')
+		a = ft_strdup(shell->env[nbe("HOME", shell)]);
+	else
+		a = ft_strdup(shell->env[nbe("PWD", shell)]);
+	splitdir = ft_split(shell->cmds->cmd_args, '/');
+	while (splitdir[x])
 	{
 		
 	}
@@ -25,25 +49,20 @@ static void	dotter(char *a, t_shell *shell)
 
 void	cder(t_shell *shell)
 {
-	char	*a;
+	int	x;
+	int	y;
 
+	x = nbe("OLDPWD", shell);
+	y = nbe("PWD", shell);
 	if (!shell->cmds->next)
 	{
-		free(shell->env[50]);
-		shell->env[50] = ft_strdup(shell->env[20]);
-		free(shell->env[20]);
-		shell->env[20] = ft_strdup(shell->env[32]);
+		free(shell->env[x]);
+		shell->env[x] = ft_strjoin(ft_strdup("OLDPWD="), variable_expansion("PWD", shell));
+		free(shell->env[y]);
+		shell->env[y] = ft_strjoin(ft_strdup("PWD="), variable_expansion("HOME", shell));
+		return ;
 	}
-	a = shell->cmds->next->cmd_args;
-	if (a[0] == '.')
-		dotter(a, shell);
-	if (access(shell->cmds->next->cmd_args, X_OK) != 0)
-	{
-		printf("directory can not be accessed or does not exist\n");
-		exit(-1);
-	}
-	free(shell->env[50]);
-	shell->env[50] = ft_strdup(shell->env[20]);
-	free(shell->env[20]);
+	shell->cmds = shell->cmds->next;
+	dotter(shell);
 }
 
